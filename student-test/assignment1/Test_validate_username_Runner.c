0f722f1cab68d657b95e232bf0ec00cad0235a86
Cameron Wood <cameronjwood7@gmail.com>
2024-03-29 21:20:42 -0400

    Updating Permissions

--- student-test/assignment1/Test_validate_username_Runner.c
+++ student-test/assignment1/Test_validate_username_Runner.c
@@ -78,7 +78,7 @@ static void run_test(UnityTestFunction func, const char* name, int line_num)
  int Test_validate_username_main(void);
 int Test_validate_username_main(void)
 {
-  UnityBegin("/home/cameron/Desktop/assignment-1-CamWood7/assignment-autotest/../student-test/assignment1/Test_validate_username.c");
+  UnityBegin("/home/cameron/Desktop/newAss1/assignment-1-CamWood7/assignment-autotest/../student-test/assignment1/Test_validate_username.c");
   run_test(test_validate_my_username, "test_validate_my_username", 15);
 
   return UnityEnd();
